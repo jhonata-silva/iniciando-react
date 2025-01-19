4e829d11d59ee0aa4d247ebe79f84ba2fa22220a
jhonata-silva <cicerojhonataoficial@gmail.com>
2025-01-19 18:13:00 -0300

    Criando aplicacao com Material UI + React

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
-import './css/style.scss';
-import logo from './assets/images/logo.webp';
+import './assets/css/style.scss';
+
+import {
+  Card, 
+  CardContent, 
+  Typography,
+  TextField,
+  Container,
+  Button} from '@mui/material';
 
 function App() {
   return (
-    <div className="row">
-      <div className=''>
-        <header className="col-12 row">
-          <div className="col-1">
-            <img src={logo} alt='logo do projeto iniciando com react'></img>
-          </div>
-          <nav className='col-11'>
-            <ul>
-              <li>Home</li>
-              <li>Cadastro</li>
-              <li>Sobre</li>
-              <li>Login</li>
-            </ul>
-          </nav>
-        </header>
-
-        <main className="col-12 row m-0">
-          <section className="col-12 col-md-8" >
-            Conteúdo
-          </section>
-          <aside className="col-12 col-md-4">
-            itens lateral
-          </aside>
-        </main>
-
-        <footer className="col-12">
-          footer
-        </footer>
-      </div>
+    <div>  
+    
     </div>
-
   );
 }
 
 export default App;
+
+
+
